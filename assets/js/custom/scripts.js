5d95de8599e9758dbb6eb7be1d59146e888dac2f
Anton <suddyadred@gmail.com>
2017-03-09 21:33:50 +0200

    Suddya

--- assets/js/custom/scripts.js
+++ assets/js/custom/scripts.js
@@ -252,5 +252,76 @@ jQuery(document).ready(function( $ ) {
 
     blogGrid();
 
+    // animate pie chart
+    var lastId,
+        topMenu = $("#top-menu"),
+        topMenuHeight = topMenu.outerHeight()+15,
+        // All list items
+        menuItems = topMenu.find("a"),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function(){
+            var item = $($(this).attr("href"));
+            if (item.length) { return item; }
+        });
+
+
+    // Bind to scroll pie charts
+    //Simple counter for presonnel.page statistic
+
+    $('.rate-box .counter').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+
+        $({ countNum: $this.text()}).animate({
+                countNum: countTo
+            },
+
+            {
+                duration: 3500,
+                easing:'linear',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                }
+
+            });
+    });
+
+    function Counter(counter, data) {
+        var $this = counter,
+            countTo = data*100;
+
+        $({ countNum: $this.text()}).animate({
+            countNum: countTo
+        },{
+            duration: 1000,
+            easing:'linear',
+            step: function() {
+                $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+                $this.text(this.countNum);
+            }
+
+        });
+    }
+
+    var ranges = $('.rate-box .rate-range');
+    $(window).scroll(function(event) {
+
+        if(ranges.visible()){
+            ranges.each(function(index, el) {
+
+
+                var data = $(el).data('percentage');
+
+                Counter($(el).siblings('.counter'), data)
+
+                $(el).css({'transform': 'scale('+ data +')'})
+            });
+        }
+    });
 
 });
